732edfa3f11d9b33f819c283c385bff471429b9e
MikoLee <lixl@tico.cn>
2020-06-02 11:23:03 +0800

    Eureka Server Demo

--- eureka-server-learning/src/main/java/com/mikolee/eurekaserverlearning/EurekaServerLearningApplication.java
+++ eureka-server-learning/src/main/java/com/mikolee/eurekaserverlearning/EurekaServerLearningApplication.java
@@ -2,14 +2,22 @@ package com.mikolee.eurekaserverlearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+//@RestController
 public class EurekaServerLearningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerLearningApplication.class, args);
     }
+//    @RequestMapping(value = "/")
+//    public String home(){
+//        return "Eureka Client application";
+//    }
 
 }
